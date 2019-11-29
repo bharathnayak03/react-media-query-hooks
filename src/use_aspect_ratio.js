a3b96bd4ca35fd362f005571d091933cb42cc05b
Bharath Nayak <bharathnayak03@gmail.com>
2019-11-29 17:46:44 +0530

    feat([core]): cleanup after eslint issues

--- src/use_aspect_ratio.js
+++ src/use_aspect_ratio.js
@@ -1,11 +1,15 @@
+// @flow
 import useMatchMedia from './use_match_media';
 
+function validateRatio(ratioStr: string) {
+  const ratio = ratioStr.split('/');
+  if (!(ratio.length !== 2 && parseInt(ratio[0], 10) && paraeInt(ratio[1], 10))) {
+    throw Error('Ratio should be in the form "16/9" ');
+  }
+}
 
-export const useAspectRatio = ratio =>
-  useMatchMedia(`(aspect-ratio: ${ratio} )`);
+export const useAspectRatio: boolean = (ratio: string) => validateRatio(ratio) && useMatchMedia(`(aspect-ratio: ${ratio} )`);
 
-export const useMinAspectRatio = ratio =>
-  useMatchMedia(`(min-aspect-ratio: ${ratio} )`);
+export const useMinAspectRatio: boolean = (ratio: string) => validateRatio(ratio) && useMatchMedia(`(min-aspect-ratio: ${ratio} )`);
 
-export const useMaxAspectRatio = ratio =>
-  useMatchMedia(`(min-aspect-ratio: ${ratio} )`);
+export const useMaxAspectRatio: boolean = (ratio: string) => validateRatio(ratio) && useMatchMedia(`(min-aspect-ratio: ${ratio} )`);
--- src/use_color_scheme.js
+++ src/use_color_scheme.js
@@ -1,7 +1,18 @@
+// @flow
 import useMatchMedia from './use_match_media';
 
-export const useDarkColorScheme = () =>
-  useMatchMedia("(prefers-color-scheme: dark)");
+export const useDarkColorScheme: boolean = () => useMatchMedia('(prefers-color-scheme: dark)');
 
-export const useLightColorScheme = () =>
-  useMatchMedia("(prefers-color-scheme: light)");
+export const useLightColorScheme: boolean = () => useMatchMedia('(prefers-color-scheme: light)');
+
+export const useColorScheme: 'light' | 'dark' | 'no-preference' = () => {
+  const isDark = useDarkColorScheme();
+  const isLight = useLightColorScheme();
+
+  if (isDark) {
+    return 'dark';
+  } if (isLight) {
+    return 'light';
+  }
+  return 'no-preference';
+};
