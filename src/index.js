58488d04fb48f22662f18fe5445585e5b28dc6e4
Bharath Nayak <bharathnayak03@gmail.com>
2019-11-29 16:54:36 +0530

    refactor(index.js): change exports

--- src/index.js
+++ src/index.js
@@ -1,6 +1,4 @@
-import useMatchMedia from './use_match_media';
-
-export * from './use_aspect_ratio'
-export * from './use_orientation'
+export { default } from './use_match_media';
+export * from './use_aspect_ratio';
+export * from './use_orientation';
 export * from './use_color_scheme'
-export default useMatchMedia;
